f634c251a893bfa7a1a4704189541095427a4b48
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-11 13:36:32 +0300

    Map urlconf for user and current user's profile views function.

--- content/urls.py
+++ content/urls.py
@@ -3,7 +3,9 @@ from django.urls import re_path, path
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('login', views.login, name='login'),
+  path('login', views.loginUser, name='login'),
   re_path('register', views.register, name='register'),
+  re_path(r'^myprofile/(?P<username>\w{0,50})/$', views.myprofile, name='myprofile'),
+  re_path(r'^userprofile/(?P<username>\w{0.50}/$)', views.userprofile, name='userprofile'),
 
 ]
