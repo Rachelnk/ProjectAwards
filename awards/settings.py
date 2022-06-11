cad13e60f42f048a104efc74f5caf3b03c5bcf4b
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-11 10:37:18 +0300

    Add media directory root path in settings.

--- awards/settings.py
+++ awards/settings.py
@@ -137,6 +137,9 @@ cloudinary.config(
   secure = True
 )
 
+# media configurations
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
