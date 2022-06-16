adaa73f668ded6089d2e5355ebe277b05a5d0112
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-16 17:30:10 +0300

    Import status module to handle status code responses.

--- content/views.py
+++ content/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import update_session_auth_hash
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProfileSerializer, PortfoioSerializer
+from rest_framework import status
 
 
 # Create your views here.
@@ -233,6 +234,13 @@ class ProfileList(APIView):
         all_profiles = Profile.objects.all()
         serializers = ProfileSerializer(all_profiles, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PortfolioList(APIView):
     def get(self, request, format=None):
