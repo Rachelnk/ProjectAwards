79d350c1eff18285cff2f47b06e88009e2f6aeb2
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-16 22:27:03 +0300

    Add settings in databse configurations.

--- awards/settings.py
+++ awards/settings.py
@@ -90,6 +90,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER' : config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
+        'HOST':'',
+        'PORT':''
     }
 }
 
