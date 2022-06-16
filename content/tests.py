1935acc8b0ecc8ce994a861a3fadf8b576b6cf43
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-16 14:43:22 +0300

    Add testcase for userprofile class model.

--- content/tests.py
+++ content/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Portfolio, Profile, Rating
 
 # Create your tests here.
+
+class TestUserProfile(TestCase):
+    def setUp(self):
+        self.new_user=User(first_name='John', last_name='Doe', username='user', email='user@gmail.com',password='user')
+        self.new_user.save()
+
+        self.profile=Profile(
+            author=self.new_user, 
+            bio='about me', 
+            profile_image='profile.jpg',
+            location = 'Kenya',
+            personal_website = 'mysite.com',
+            github_link = 'github.com',
+            instagram_link = 'instagram.com',
+            linkedin_link = 'linkedin.com',
+            twitter_link = 'twitter.com'
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
